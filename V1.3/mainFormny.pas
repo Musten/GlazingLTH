c13ecaee5884f3142f4b167a039c3555ab966e47
Kvanbanan <khoanzzzz@hotmail.com>
2014-10-20 17:22:21 +0200

    hallo

--- V1.3/mainFormny.pas
+++ V1.3/mainFormny.pas
@@ -1,7 +1,7 @@
 unit mainFormny;
 interface
-
 uses
+//Hallo
   System.SysUtils, System.Types, System.UITypes, System.Classes,
   System.Variants,
   FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
